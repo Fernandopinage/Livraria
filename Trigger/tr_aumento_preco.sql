38e3f6e3f15b7fe8a9eca009b479ee1f86c5ffbb
Fernando Pinage <luizfernandoluck@hotmail.com>
2020-05-02 14:13:56 -0300

    criação da trigger

---
+++ Trigger/tr_aumento_preco.sql
@@ -0,0 +1,13 @@
+delimiter // 
+
+CREATE TRIGGER tr_desconto_preco BEFORE INSERT on edicoes 
+
+for EACH row 
+
+BEGIN 
+
+set new.preco = (preco + 5); 
+
+INSERT INTO `edicoes`( `id_editora`, `id_livro`, `preco`, `num_pag`, `qtde`) VALUES (2,1,'80,50',632,10); 
+
+END // 
